6313fbd80389b1f197b5843a64bdf925d6be96c2
camilo-OG <camilo.osorio.gsach.cl>
2023-08-31 11:48:40 -0400

    prueba 1

--- src/components/pages/productos/Productos.jsx
+++ src/components/pages/productos/Productos.jsx
@@ -3,9 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import BotonesProductos from "./BotonesProductos"
 import BannerProductos from "./BannerProductos"
 import Footer from "../Footer/Footer"
-import NavBarPage from "../navbar/NavBarPage"
-import { Link } from "react-router-dom"
 
+import { Link } from "react-router-dom"
+import NavBarPage from "../navbar/NavBarPage";
 
 function Productos () {
   return (
